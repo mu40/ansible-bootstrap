42d2f9edfc50cc0dd7899a845d0b94cc79256a74
Malte Hoffmann <mhoffmann@mgh.harvard.edu>
2025-08-06 21:15:17 -0400

    Set up environment, improve YAML.

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Setup up virtual Python envirironmet for linting and Git hooks.
+
+ENV='.venv'
+
+
+# Virtual environment.
+if [ ! -d "$ENV" ]; then
+    python3 -m venv "$ENV"
+    . "$ENV/bin/activate"
+
+    # Packages. Package passlib required for `password_hash` filter.
+    pip install -U pip setuptools wheel
+    pip install shellcheck-py typos
+fi
+
+
+# Hooks.
+d="hooks"
+[ -d "$d" ] && cp -v "$d"/* ".git/$d"
+
+
+# Environment manager.
+cat >.envrc <<EOF
+[ -d "$ENV" ] && . "$ENV/bin/activate"
+EOF
